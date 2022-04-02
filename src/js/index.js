b478b9c1386ac07d45c9a5e0b5ef422fc8e7030b
Gabriel dos Santos <gsantos.4dev@gmail.com>
2022-04-01 23:23:13 -0300

    Grande atualização
    Adicionando mais códigos no js, fazendo as animações de popup nos cartões dos "especiais do dia".
    
    Adicionando mais um arquivo popup.css com a estilização do popup.
    
    Adicionando novas divs ao index.html

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
+// CARDS
 let count2 = 5
 let counte21 = 1
 function counte2(){
@@ -28,3 +29,49 @@ function cima(){
     document.querySelector('.c-'+count).classList.add('aberto')
     document.querySelector('.c-'+counte1).classList.remove('aberto')
 }
+// POPUP
+const popup1 = document.querySelector('.popup-c-1')
+const cafe1 = document.querySelector('.c-1')
+cafe1.addEventListener( 'click', () =>{
+    popup.style.display = 'block'
+    popup1.style.display = 'block'
+})
+const popup2 = document.querySelector('.popup-c-2')
+const cafe2 = document.querySelector('.c-2')
+cafe2.addEventListener( 'click', () =>{
+    popup.style.display = 'block'
+    popup2.style.display = 'block'
+})
+const popup3 = document.querySelector('.popup-c-3')
+const cafe3 = document.querySelector('.c-3')
+cafe3.addEventListener( 'click', () =>{
+    popup.style.display = 'block'
+    popup3.style.display = 'block'
+})
+const popup4 = document.querySelector('.popup-c-4')
+const cafe4 = document.querySelector('.c-4')
+cafe4.addEventListener( 'click', () =>{
+    popup.style.display = 'block'
+    popup4.style.display = 'block'
+})
+const popup5 = document.querySelector('.popup-c-5')
+const cafe5 = document.querySelector('.c-5')
+cafe5.addEventListener( 'click', () =>{
+    popup.style.display = 'block'
+    popup5.style.display = 'block'
+})
+const popup = document.querySelector('.popup-box')
+popup.addEventListener( 'click', event =>{
+    const classClick = event.target.classList[0]
+    const classNames = ['popup-box', 'popup-close', 'popup-button']
+    const close = classNames.some( classNames => classNames === classClick)
+    
+    if (close) {
+        popup.style.display = 'none'
+        popup1.style.display = 'none'
+        popup2.style.display = 'none'
+        popup3.style.display = 'none'
+        popup4.style.display = 'none'
+        popup5.style.display = 'none'
+    }
+})
